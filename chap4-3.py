563155167842b1de9cae61e23bdb70337537c5c8
kuma <kuma@augusta-lab.co.jp>
2023-06-17 17:53:52 +0900

    ボタンクリックイベントの追加

--- chap4-3.py
+++ chap4-3.py
@@ -30,4 +30,31 @@ entry.place(x=200, y=350)
 button = tk.Button(text="決定")
 button.place(x=380, y=350)
 
+# 仲間を選択したときの処理
+def choice_friend(friend: str, img: tk.PhotoImage):
+    sys_text.destroy()
+    res_text = tk.Label(text=f"{friend}が仲間になりました。")
+    res_text.place(x=200, y=60)
+    canvas.delete("illust")
+    canvas.create_image(320, 220, image=img, tag="illust")
+
+# ボタンクリックイベント
+def click_btn():
+    val = float(entry.get())
+    if val == 1:
+        choice_friend("王様", king_img)
+    elif val == 2:
+        choice_friend("魔物A", monsterA_img)
+    elif val == 3:
+        choice_friend("魔物B", monsterB_img)
+    else:
+        res_text = tk.Label(text="だれも仲間になりませんでした。")
+        res_text.place(x=160, y=60)
+        canvas.delete("illust")
+
+    button["state"] = "disabled"
+    entry["state"] = "disabled"
+
+button["command"] = click_btn
+
 root.mainloop()
