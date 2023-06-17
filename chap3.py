f65e266e94c2775e18867c4b8c693089c92fdfd1
kuma <kuma@augusta-lab.co.jp>
2023-06-17 16:26:18 +0900

    テキストの表示

--- chap3.py
+++ chap3.py
@@ -4,12 +4,17 @@ import tkinter as tk
 root = tk.Tk()
 root.title("リリーにしつもん")
 root.minsize(640, 480)
+root.option_add("*font", ["MS Pゴシック", 22])
 
 # 画像表示
 canvas = tk.Canvas(bg="black", width=640, height=480)
 canvas.place(x=0, y=0)
 img = tk.PhotoImage(file="img3/chap3-back.png")
 canvas.create_image(320, 240, image=img)
+
+# テキスト表示
+question = tk.Label(text="知りたいのは何分かな？", bg="white")
+question.place(x=100, y=40)
 
 # メインループ
 root.mainloop()
