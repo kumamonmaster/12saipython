eb5d3e8646bb10a6dbb99af3da28f4b30825339f
kuma <kuma@augusta-lab.co.jp>
2023-06-17 16:22:05 +0900

    画像の表示

--- chap3.py
+++ chap3.py
@@ -5,5 +5,11 @@ root = tk.Tk()
 root.title("リリーにしつもん")
 root.minsize(640, 480)
 
+# 画像表示
+canvas = tk.Canvas(bg="black", width=640, height=480)
+canvas.place(x=0, y=0)
+img = tk.PhotoImage(file="img3/chap3-back.png")
+canvas.create_image(320, 240, image=img)
+
 # メインループ
 root.mainloop()
