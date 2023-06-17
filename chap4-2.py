6c19599be1af85119636e08c416a7f579acdf5d9
kuma <kuma@augusta-lab.co.jp>
2023-06-17 17:07:42 +0900

    画像の読み込みとキャンバスの作成

--- chap4-2.py
+++ chap4-2.py
@@ -6,5 +6,15 @@ root.title("勇者求む！")
 root.minsize(640, 480)
 root.option_add("*font", ["メイリオ", 14])
 
+# 画像読み込み
+img1 = tk.PhotoImage(file="img4/chap4-1-1.png")
+img2 = tk.PhotoImage(file="img4/chap4-1-2.png")
+img3 = tk.PhotoImage(file="img4/chap4-1-3.png")
+
+# キャンバス作成
+canvas = tk.Canvas(root, width=640, height=480)
+canvas.place(x=0, y=0)
+canvas.create_image(320, 220, image=img1, tag="illust")
+
 # メインループ
 root.mainloop()
