1deaed3cae42fe5f80707afebc36bf1239679e95
kuma <kuma@augusta-lab.co.jp>
2023-06-17 17:10:56 +0900

    ラベルの配置

--- chap4-2.py
+++ chap4-2.py
@@ -16,5 +16,12 @@ canvas = tk.Canvas(root, width=640, height=480)
 canvas.place(x=0, y=0)
 canvas.create_image(320, 220, image=img1, tag="illust")
 
+# ラベル配置
+serifu_text = tk.Label(text="王様「魔王を倒したら褒美をやるぞ！」")
+serifu_text.place(x=160, y=10)
+
+sys_text = tk.Label(text="褒美はいくらあげますか？", fg="red")
+sys_text.place(x=180, y=380)
+
 # メインループ
 root.mainloop()
