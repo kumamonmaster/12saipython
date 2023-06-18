c8c7d716daf6d82d9e7d89ead60509de34b995a9
kuma <kuma@augusta-lab.co.jp>
2023-06-18 12:45:26 +0900

    画像ファイルの追加とウィンドウの作成

---
+++ chap6.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+
+# ウィンドウの作成
+root = tk.Tk()
+root.title("ダンジョン＆パイソン")
+root.minsize(840, 454)
+root.option_add("*font", ["メイリオ", 14])
+
+# キャンバス作成
+canvas = tk.Canvas(width=620, height=434)
+canvas.place(x=10, y=10)
+canvas.create_rectangle(0, 0, 620, 434, fill="gray")
+
+root.mainloop()
