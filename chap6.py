288111a180761070e2c608fd9bdaf912b43b851e
kuma <kuma@augusta-lab.co.jp>
2023-06-18 12:47:48 +0900

    画像の読み込み

--- chap6.py
+++ chap6.py
@@ -9,6 +9,15 @@ root.option_add("*font", ["メイリオ", 14])
 # キャンバス作成
 canvas = tk.Canvas(width=620, height=434)
 canvas.place(x=10, y=10)
-canvas.create_rectangle(0, 0, 620, 434, fill="gray")
+canvas.create_rectangle(0, 0, 620, 434, fill="gray", tag="drawField")
+
+# 画像読み込み
+images = [
+    tk.PhotoImage(file="img6/chap6-mapfield.png"),
+    tk.PhotoImage(file="img6/chap6-mapwall.png"),
+    tk.PhotoImage(file="img6/chap6-mapgoal.png"),
+    tk.PhotoImage(file="img6/chap6-mapkey.png"),
+    tk.PhotoImage(file="img6/chap6-mapman.png")
+]
 
 root.mainloop()
