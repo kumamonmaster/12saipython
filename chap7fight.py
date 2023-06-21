90bc8806fda7d2772f8d48c0c52ea37e9d159b8e
kuma <kuma@augusta-lab.co.jp>
2023-06-21 17:41:06 +0900

    戦闘敗北処理とCharacterクラスの追加

--- chap7fight.py
+++ chap7fight.py
@@ -1,3 +1,4 @@
+import random
 import tkinter as tk
 
 
@@ -18,6 +19,7 @@ class FightManager:
         # 「負けた」ボタン
         btn_lose = tk.Button(self.dialog, text="負けた")
         btn_lose.place(x=320, y=340)
+        btn_lose["command"] = self.fight_lose
 
         # 非表示
         self.dialog.place_forget()
@@ -33,3 +35,35 @@ class FightManager:
         """戦闘勝利"""
         self.map_data[self.brave_y][self.brave_x] = 0
         self.dialog.place_forget()
+
+    def fight_lose(self):
+        """戦闘敗北"""
+        canvas = tk.Canvas(self.dialog, width=820, height=434)
+        canvas.place(x=0, y=0)
+        canvas.create_rectangle(0, 0, 620, 434, fill="red")
+        canvas.create_text(300, 200, fill="white", font=("MS ゴシック", 15), text="""勇者は負けてしまった。
+最初からやり直してくれたまえ。""")
+
+
+class Character:
+    """キャラクタークラス"""
+    def get_atk(self):
+        """攻撃力を返す"""
+        return random.randint(1, self.atk)
+
+    def get_dfs(self):
+        """防御力を返す"""
+        return random.randint(0, self.dfs)
+
+    def culc_hp(self, atk, dfs):
+        dmg = atk - dfs
+
+        if dmg < 1:
+            return self.hp
+
+        self.hp -= dmg
+
+        if self.hp < 1:
+            self.hp = 0
+
+        return self.hp
