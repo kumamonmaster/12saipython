17cf8ab13016c0021db9df5db7172f8528b167e1
kuma <kuma@augusta-lab.co.jp>
2023-06-21 17:16:13 +0900

    キャンバスとボタンの配置

--- chap7fight.py
+++ chap7fight.py
@@ -6,5 +6,15 @@ class FightManager:
         self.dialog = tk.Frame(width=820, height=434)
         self.dialog.place(x=10, y=10)
 
+        canvas = tk.Canvas(self.dialog, width=820, height=434)
+        canvas.place(x=0, y=0)
+        canvas.create_rectangle(0, 0, 620, 434, fill="black")
+
+        # ボタン作成
+        btn_win = tk.Button(self.dialog, text="勝った")
+        btn_win.place(x=180, y=340)
+        btn_lose = tk.Button(self.dialog, text="負けた")
+        btn_lose.place(x=320, y=340)
+
         # 非表示
         self.dialog.place_forget()
